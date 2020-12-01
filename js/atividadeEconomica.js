d5eaeddd89addcbf33e786881457dbefb643159c
betosjesus <37048513+betosjesus@users.noreply.github.com>
2020-12-01 20:16:52 -0300

    Grafico dinamico adicionado e icon correspondente

--- js/atividadeEconomica.js
+++ js/atividadeEconomica.js
@@ -12,14 +12,19 @@ function trocarParaTabela(){
 	var numeroDoGrafico = sessionStorage.getItem("numero_Do_Grafico"); 
 	var string1 = "div_" + numeroDoGrafico;
 	var string2 = "tabela_" + numeroDoGrafico;
+	var string3 = "garficoDinamico_" + numeroDoGrafico;
 	const grafico = document.getElementById(string1);
 	const tabela = document.getElementById(string2);
+	const garficoDinamico = document.getElementById(string3);
 	var icon_tab = document.getElementById("icon-tabela");
 	var icon_graf = document.getElementById("icon-grafico");
+	var icon_grafDinamico = document.getElementById("icon-graficoDinamico");
 
 	icon_tab.style.display = "none";
 	icon_graf.style.display = "inline";
+	icon_grafDinamico.style.display = "inline";
 
+	garficoDinamico.style.display = "none";
 	grafico.style.display = "none";
 	tabela.style.display = "inline";
 }
@@ -28,17 +33,45 @@ function trocarParaGrafico(){
 	var numeroDoGrafico = sessionStorage.getItem("numero_Do_Grafico"); 
 	var string1 = "div_" + numeroDoGrafico;
 	var string2 = "tabela_" + numeroDoGrafico;
+	var string3 = "garficoDinamico_" + numeroDoGrafico;
 	const grafico = document.getElementById(string1);
 	const tabela = document.getElementById(string2);
+	const garficoDinamico = document.getElementById(string3);
 	var icon_tab = document.getElementById("icon-tabela");
 	var icon_graf = document.getElementById("icon-grafico");
+	var icon_grafDinamico = document.getElementById("icon-graficoDinamico");
 
 	icon_tab.style.display = "inline";
 	icon_graf.style.display = "none";
+	icon_grafDinamico.style.display = "inline";
 
+	garficoDinamico.style.display = "none";
 	grafico.style.display = "inline";
 	tabela.style.display = "none";
 }
+
+function trocarParaGraficoDinamico(){
+	var numeroDoGrafico = sessionStorage.getItem("numero_Do_Grafico"); 
+	var string1 = "div_" + numeroDoGrafico;
+	var string2 = "tabela_" + numeroDoGrafico;
+	var string3 = "garficoDinamico_" + numeroDoGrafico;
+	const grafico = document.getElementById(string1);
+	const tabela = document.getElementById(string2);
+	const garficoDinamico = document.getElementById(string3);
+	var icon_tab = document.getElementById("icon-tabela");
+	var icon_graf = document.getElementById("icon-grafico");
+	var icon_grafDinamico = document.getElementById("icon-graficoDinamico");
+
+	icon_tab.style.display = "inline";
+	icon_graf.style.display = "inline";
+	icon_grafDinamico.style.display = "none";
+
+	garficoDinamico.style.display = "inline";
+	grafico.style.display = "none";
+	tabela.style.display = "none";
+}
+
+
 
 function downloadCsv(){
 	var numeroDoGrafico = sessionStorage.getItem("numero_Do_Grafico");
